1aadb707f260ee1b17ee1b71f6d2dc32247cc356
unknown <daniel.madrid-garrabe@epitech.eu>
2025-11-30 17:56:59 +0100

    a

--- src/components/VerificationCodeInput.tsx
+++ src/components/VerificationCodeInput.tsx
@@ -109,7 +109,7 @@ const VerificationCodeInput: React.FC<VerificationCodeInputProps> = ({
         {code.map((digit, index) => (
           <input
             key={index}
-            ref={(el) => (inputRefs.current[index] = el)}
+            ref={(el) => { inputRefs.current[index] = el; }}
             type="text"
             inputMode="numeric"
             maxLength={1}
